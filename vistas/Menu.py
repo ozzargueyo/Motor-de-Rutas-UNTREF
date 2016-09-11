8e6e4ff6fa90bb9ef23c6e7fdcbb7116e1fcc4b3
Oscar Andrés Argueyo <ozz.rokr@hotmail.com>
2016-09-11 01:40:16 -0300

    organizando un poco el menu

--- vistas/Menu.py
+++ vistas/Menu.py
@@ -1,4 +1,6 @@
 from controladores import motorDeRutas
+from terminaltables import AsciiTable, DoubleTable
+import os
 
 class Menu(object):
 
@@ -6,21 +8,55 @@ class Menu(object):
         #El menu se inicializa con el flag en terminar como False
         self.terminar = False
         self.motor = motorDeRutas.MotorDeRutas()
-        self.operaciones = ["Crear trayecto", "Agregar ciudad a trayecto existente", "Listar trayectos",
-                            "Obtener informacion de trayecto", "Comparar trayectos", "Salir"]
-        self.menus_disponibles = [self.crear_trayecto, self.agregar_ciudad, self.motor.listar_trayectos,
-                                  self.info_trayecto, self.comparar_trayectos, self.motor.salir_y_guardar_trayectos]
+        self.operaciones = self.definirOperaciones()
+        self.menus_disponibles = self.operacionesDisponibles()
+
+    def definirOperaciones(self):
+
+        return {
+            1 : "Crear trayecto" ,
+            2 : "Agregar ciudad a trayecto existente",
+            3 : "Listar trayectos",
+            4 : "Obtener informacion de trayecto",
+            5 : "Comparar trayectos",
+            6 : "Salir",
+        }
+
+    def operacionesDisponibles(self):
+        return {
+            1:  self.crear_trayecto,
+            2:  self.agregar_ciudad,
+            3:  self.listar_trayectos,
+            4:  self.info_trayecto,
+            5:  self.comparar_trayectos,
+            6:  self.motor.salir_y_guardar_trayectos
+        }
+
 
     def elegir_operacion(self):
         """PREGUNTA AL USUARIO QUE FUNCIONALIDAD REQUIERE"""
-        self.menus_disponibles[self.seleccion_por_numero(self.operaciones, "operacion")]()
+        try:
+            op = int(self.seleccionMenu(self.operaciones, "operación"))
+            print("--------------------------------------------------")
+            print("\nOperacion seleccionada:" , self.operaciones[op] , "\n" )
+            self.menus_disponibles[op]()
+
+            if op != 6:
+                print("¿Desea continuar operando con el Motor de Rutas?"
+                                          "\n Oprima [Enter] para continuar o [N] para salir:" )
+                continuarConMotor = input()
+                if continuarConMotor == "N":
+                    self.menus_disponibles[6]()
+        except :
+            print("Ocurrio un error")
 
     def crear_trayecto(self):
         """MENU PARA CREAR UN NUEVO TRAYECTO"""
-        nombre = input("Inserte nombre del nuevo trayecto \n")
+        nombre = input("Inserte nombre del nuevo trayecto: ")
         origen = self.obtener_nombre_de_ciudad("de origen")
         destino = self.obtener_nombre_de_ciudad("de destino")
         self.motor.crear_trayecto(nombre, origen, destino)
+        self.clear()
 
     def agregar_ciudad(self):
         """MENU PARA AÑADIR CIUDAD"""
@@ -35,7 +71,23 @@ class Menu(object):
 
     def info_trayecto(self):
         """MENU PARA OBTENER INFORMACION DE UN TARYECTO SELECCIONADO"""
-        self.motor.ver_trayecto(self.seleccionar_trayecto())
+        #self.motor.ver_trayecto(self.seleccionar_trayecto())
+        op = self.seleccionar_trayecto()
+        self.motor.ver_trayecto(op)
+        #self.motor.seleccionarTrayecto()
+
+    def listar_trayectos(self):
+
+        table_data = [["Nro" , "Nombre del Trayecto"]]
+        index = 1
+        for valor in sorted(self.motor.trayectos.keys()):
+            row = [ index , valor ]
+            table_data.append(row)
+            index += 1
+
+        table = DoubleTable(table_data, "Trayectos almacenados ")
+        table.justify_columns = {0: 'center', 1: 'left'}
+        print(table.table)
 
     def comparar_trayectos(self):
         """MENU PARA COMPARAR DOS TRAYECTOS EXISTENTES"""
@@ -53,23 +105,52 @@ class Menu(object):
             while ("si" not in seleccion and "no" not in seleccion):
                 seleccion = input(
                     'Escriba "si" si la ciudad que usted eligio es: ' + nombre + ' o esciba "no" para reescribir el nombre \n').casefold()
-                print(seleccion)
                 ciudad_incorrecta = "si" not in seleccion
         return nombre
 
     def seleccionar_trayecto(self):
+
         trayectos = []
         for t in self.motor.trayectos.keys():
             trayectos.append(t)
-        return trayectos[self.seleccion_por_numero(trayectos, "trayecto")]
 
-    def seleccion_por_numero(self, lista, texto):
+        op = self.seleccionDeTrayectos(trayectos, "  Seleccionar Trayectos  ")
+        return trayectos[op]
+
+    def seleccionMenu(self, opciones, texto):
         posicion = -1
         while posicion == -1:
-            for i in range(0, len(lista)):
-                print(str(i + 1) + len(str(i + 1)) * " " + lista[i])
+            print("------------------------------------------------")
+            print("Motor de Rutas", " - " ,"Operaciones Disponibles")
+            for clave, valor in sorted(opciones.items()):
+                print("\t ["+str(clave)+"] - ", valor)
+            print("------------------------------------------------")
             try:
-                numero = int(input("Inserte número del " + texto + " que desea \n"))
+                numero = int(input("\nInserte número de la " + texto + " que desea: "))
+                if numero >= 1 and len(opciones) >= numero:
+                    posicion = numero
+                else:
+                    self.clear()
+                    print("Debe ingresar un numero entre 1 y " + str(len(opciones)))
+            except:
+                print("Debe ingresar un numero entre 1 y " + str(len(opciones)))
+        return posicion
+
+    def seleccionDeTrayectos(self, lista, title):
+        posicion = -1
+        table_data = [["Nro" , "Nombre del Trayecto"]]
+        index = 1
+        for valor in lista:
+            row = [ index , valor ]
+            table_data.append(row)
+            index += 1
+
+        table = DoubleTable(table_data, title)
+        table.justify_columns = {0: 'center', 1: 'left'}
+        print(table.table)
+        while posicion == -1:
+            try:
+                numero = int(input("Ingese la opción que desea:"))
                 if numero >= 1 and len(lista) >= numero:
                     posicion = numero - 1
                 else:
@@ -78,9 +159,14 @@ class Menu(object):
                 print("Debe ingresar un numero entre 1 y " + str(len(lista)))
         return posicion
 
+
     def mensajeDeSalida(self):
         print("Gracias por usar el Motor de Rutas UNTREF")
         exit()
+
+    def clear(self):
+        os.system('clear')
+
 
 
 menu = Menu()
