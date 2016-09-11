74ad44ed432a874efdcd7c0bad4db9995c082e9e
Oscar Andrés Argueyo <ozz.rokr@hotmail.com>
2016-09-11 02:27:23 -0300

    Path de files arreglado Hay que usar os.path.join sin las barras para que el propio interprete una el path del archivo con las barras correctas

--- modelos/Model.py
+++ modelos/Model.py
@@ -3,7 +3,7 @@ class Model(object):
     def __init__(self , collection):
         self.id = None
         dir_path = os.path.dirname(__file__)
-        self.collection = os.path.join(dir_path , "../storage/"+collection+".json")
+        self.collection = os.path.join(dir_path, "..", "storage", collection+".json")
 
     def get(self):
         return json.load(open(self.collection, "r"))
